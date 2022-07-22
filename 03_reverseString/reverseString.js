aae4316a2c7b9a1269639f361a70cb5f8ad06534
jpfairhart <johnpaulfairhart@hotmail.com>
2022-07-22 14:58:46 -0700

    initial solution prior to testing

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let str = string;
+    let stringArray = [];
+    let revArray = [];
+    for (i = 0; i < str.length; i++) {
+        stringArray[i] = str.charAt(i);
+    }
+    revArray = stringArray.reverse();
+    return revArray.toString();
 };
 
 // Do not edit below this line
