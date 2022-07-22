f89eb40ed7c57f31a3c892257d987a59a9435333
jpfairhart <johnpaulfairhart@hotmail.com>
2022-07-21 22:02:23 -0700

    added repeatString function to test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let fullString;
+    for (i = 0; i < num; i++) {
+        return fullString + string;
+    }
 };
-
 // Do not edit below this line
 module.exports = repeatString;
