5586a85f33b0e52e0fb914ff219b9641129f8144
jpfairhart <johnpaulfairhart@hotmail.com>
2022-07-23 08:33:45 -0700

    solution passes all tests when accounting for negatives and types

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,30 @@
 const sumAll = function(int1, int2) {
-    let sum = int1;
-    for (i = (int1 + 1); i <= int2; i++) {
-        sum += i;
+    if ((typeof int1 !== "number") || (typeof int2 !== "number")) {
+        return "ERROR";
     }
-    return sum;
+    else if ((int1 < 0) || (int2 < 0)) {
+        return "ERROR";
+    }
+    else if (int1 < int2) {
+        let sum = int1;
+        let greater = int2;
+        for (i = (sum + 1); i <= greater; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    else if (int2 < int1) {
+        let sum = int2;
+        let greater = int1;
+        for (i = (sum + 1); i <= greater; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    else {
+        let sum = (int1 + int2);
+        return sum;
+    }
 };
 
 // Do not edit below this line
