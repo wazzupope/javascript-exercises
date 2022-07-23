90671e75bcaa2e47c534049ae9079d436e12f136
jpfairhart <johnpaulfairhart@hotmail.com>
2022-07-22 23:19:22 -0700

    change start point for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(int1, int2) {
     let sum = int1;
-    for (i = int1; i <= int2; i++) {
+    for (i = (int1 + 1); i <= int2; i++) {
         sum += i;
     }
     return sum;
