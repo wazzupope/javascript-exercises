4585042a369284413a0b3bf2b449d38a1f04a2d4
jpfairhart <johnpaulfairhart@hotmail.com>
2022-07-22 23:01:03 -0700

    first attempt at solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let sum = int1;
+    for (i = int1; i <= int2; i++) {
+        sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
