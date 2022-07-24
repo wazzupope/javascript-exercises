fb71a69535885ad829a9890f750f7cedf22eb62a
jpfairhart <johnpaulfairhart@hotmail.com>
2022-07-24 14:18:23 -0700

    initial attempt at solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+  let celsius = (ftemp - 32) * (5/9);
+  return Math.round(celsius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+  let fahr = (ctemp * (9/5)) + 32;
+  return Math.round(fahr * 10) / 10;
 };
 
 // Do not edit below this line
