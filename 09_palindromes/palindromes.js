751c29fbb2b027a5ce58b2f6ec238f5497753dcf
jpfairhart <johnpaulfairhart@hotmail.com>
2022-09-22 22:31:13 -0700

    add pseudocode on comments

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,5 +2,15 @@ const palindromes = function () {
 
 };
 
+/*
+remove punctuation and spaces from String
+to lower case
+convert string to array
+get length of array
+create variable within loop that updates i-- from array length and compares to the i++ until they are equal
+if all true, return true
+if ever false, stop and return false
+*/
+
 // Do not edit below this line
 module.exports = palindromes;
