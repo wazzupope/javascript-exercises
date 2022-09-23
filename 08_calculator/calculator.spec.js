56c74aedd54aa2528234b631568322193261b397
jpfairhart <johnpaulfairhart@hotmail.com>
2022-09-22 22:09:02 -0700

    remove first skip

--- 08_calculator/calculator.spec.js
+++ 08_calculator/calculator.spec.js
@@ -5,7 +5,7 @@ describe('add', () => {
 		expect(calculator.add(0,0)).toBe(0);
 	});
 
-	test.skip('adds 2 and 2', () => {
+	test('adds 2 and 2', () => {
 		expect(calculator.add(2,2)).toBe(4);
 	});
 
