158c736c924bdda6cd589770dcd462c6ac9142e3
jpfairhart <johnpaulfairhart@hotmail.com>
2022-09-25 21:46:15 -0700

    update pseudocode in comments

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -8,6 +8,13 @@ to lower case
 convert string to array
 get length of array
 create variable within loop that updates i-- from array length and compares to the i++ until they are equal
+
+create variable for array[0] and also array[array.length -1]
+
+is array[0] === array[array.length -1]
+
+iterate through loop
+
 if all true, return true
 if ever false, stop and return false
 */
