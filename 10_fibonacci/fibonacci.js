b4901b75c5b01aab67e6218aec55cedc615c6347
jpfairhart <johnpaulfairhart@hotmail.com>
2022-10-10 21:33:15 -0700

    initial attempt at solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let prevFibo = 1;
+    let newFibo = 1;
+    for (i = 0; i < num; i++) {
+        if (num === 1) {
+            return (prevFibo + newFibo);
+        }
+        else if (num < 1) {
+            return 1;
+        }
+        else {
+            newFibo += prevFibo;
+            if ((num - 1) === 1) {
+                return newFibo;
+            }
+            else {
+                prevFibo = newFibo;
+            };
+        };
+    };
 };
 
 // Do not edit below this line
