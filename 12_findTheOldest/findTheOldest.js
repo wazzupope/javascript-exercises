7f05a58fd2ad13375535878985f084aedf2f2d82
jpfairhart <johnpaulfairhart@hotmail.com>
2022-10-11 22:47:57 -0700

    initial attempt, still need to find a way to compare ages and return oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let array = people;
+    const today = newDate();
+    const year = today.getFullYear();
+    array.forEach(function (element) {
+        if (array.yearOfDeath == undefined) {
+            element.age = (year - element.yearOfBirth);
+        }
+        else {
+            element.age = (element.yearOfDeath - element.yearOfBirth);
+        };
+    });
+    
 };
 
 // Do not edit below this line
