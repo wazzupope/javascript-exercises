ff784128c0c83cab45e21c548d4135b85b39f552
jpfairhart <johnpaulfairhart@hotmail.com>
2022-10-11 21:47:22 -0700

    initial attempt at solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let bookArr = books;
+    let titles = bookArr.map((book) => {
+        return title: book.title;
+    });
+    return titles;
 };
 
 // Do not edit below this line
