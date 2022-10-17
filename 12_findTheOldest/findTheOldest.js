a1d7c371c06ed459b098fb6358c7783533e7fc65
jpfairhart <johnpaulfairhart@hotmail.com>
2022-10-16 21:57:34 -0700

    initial full solution, not yet tested or debugged

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -2,15 +2,19 @@ const findTheOldest = function(people) {
     let array = people;
     const today = newDate();
     const year = today.getFullYear();
+    let ages = [];
     array.forEach(function (element) {
-        if (array.yearOfDeath == undefined) {
+        if (element.yearOfDeath == undefined) {
             element.age = (year - element.yearOfBirth);
         }
         else {
             element.age = (element.yearOfDeath - element.yearOfBirth);
         };
     });
-    
+    for (i = 0; i < array.length; i++) {
+        ages.push(array[i].age);
+    };
+    return Math.max(ages);
 };
 
 // Do not edit below this line
