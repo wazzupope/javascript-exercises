e6dfc1a43fdbeb740ca1641f1765fe420720d35a
jpfairhart <johnpaulfairhart@hotmail.com>
2022-10-16 22:42:05 -0700

    hand typed solution while referencing correct answer to help in understanding how to use reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,19 @@
 const findTheOldest = function(people) {
     let array = people;
-
+    return array.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    });
 };
+
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    };
+    return death - birth;
+};
+
     /*const today = new Date();
     const year = today.getFullYear();
     let ages = [];
